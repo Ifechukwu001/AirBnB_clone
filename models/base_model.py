c9cfc50979636579bf42f2ebfdc9a42bdf0b9e99
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2022-09-01 16:43:12 +0100

    Implemented creating a new class from a dictionary and added documentation.

---
+++ models/base_model.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+"""A file that contains the BaseModel class
+"""
+from uuid import uuid4
+from datetime import datetime
+
+
+class BaseModel():
+    """BaseModel class
+    """
+    def __init__(self, *args, **kwargs):
+        """ init method.
+
+        Args:
+            args (:obj:`tuple`): Not used.
+            kwargs (:obj:`dict`): Keyword arguments for initializing an object.
+
+        Attributes:
+            id (str): Unique identification of the object.
+            created_at (:obj:`datetime.datetime`): Date_time object was created
+            updated_at (:obj:`datetime.datetime`): Date_time object was updated
+
+        """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.fromisoformat(value)
+                self.__setattr__(key, value)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+    def __str__(self):
+        """ str method.
+
+        Returns:
+            str: String representation of the object.
+
+        """
+        class_name = self.__class__.__name__
+        return "[{}] ({}) ({})" .format(class_name, self.id, self.__dict__)
+
+    def save(self):
+        """ Save method.
+
+        Updates the attribute 'updated_at'
+
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """ Gets a dictionary representation of the object.
+
+        Returns:
+            :obj:`dict`: Dictionary represntation.
+
+        """
+        class_name = self.__class__.__name__
+        new_dict = self.__dict__
+        new_dict.update(__class__=class_name)
+        new_dict.update(created_at=self.created_at.isoformat())
+        new_dict.update(updated_at=self.updated_at.isoformat())
+        return new_dict
